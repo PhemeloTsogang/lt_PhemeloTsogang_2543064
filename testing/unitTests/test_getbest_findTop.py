03d61ffae2669d6001e086cbae45f6c02133d680
Phemelo Tsogang <2543064@students.wits.ac.za>
2026-04-17 18:32:07 +0200

    All bugs in testing files fixed

--- testing/unitTests/test_getbest_findTop.py
+++ testing/unitTests/test_getbest_findTop.py
@@ -9,20 +9,20 @@ class TestFindTop(unittest.TestCase):
         f = open("testing/testData/test_data_findTop_normal.csv") #Opens the data file and stores the header in a variable
         num_col, mark_col = code.getCols(f) # Acquires the student number and corresponding mark columns
         best_idx, best = code.findTop(f, num_col, mark_col) #Acquires the highest mark in the mark column and the coresponding student number
-        f.Close()
+        f.close()
 
         self.assertEqual(best_idx, "200001")
-        self.assertEqual(best, 900)
+        self.assertEqual(best, 90)
 
     # Tests if the function works correctly if the first student has the highest mark
-    def test_top_student_first(self)::
+    def test_top_student_first(self):
         f = open("testing/testData/test_data_findTop_first.csv")
         num_col, mark_col = code.getCols(f)
         best_idx, best = code.findTop(f, num_col, mark_col)
         f.close()
 
         self.assertEqual(best_idx, "200000")
-        self.assertEqual(best, 822)
+        self.assertEqual(best, 82)
 
     # Tests is the function still works correctly if there is only one student in the data set
     def test_single_student(self):
@@ -35,6 +35,6 @@ class TestFindTop(unittest.TestCase):
         self.assertEqual(best, 72)
 
 
-if __name__ == "___main___";
+if __name__ == "___main___":
     # Runs all the test in the file when executed
     unittest.main()
